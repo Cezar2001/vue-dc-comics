113e4a986a8d277223f17ed1d523eeef2a027f2e
Cezar <cezarlantosgmail.com>
2022-01-28 12:57:07 +0100

    cards

--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -1,19 +1,37 @@
 <template>
-    <div>Domani</div>
+    <div class="container">
+
+        <div v-for="(card, index) in cards" :key="index" class="cards">
+                <img :src="card.thumb"/>
+                <h3>{{card.series}}</h3>
+        </div>
+
+    </div>
 </template>
 
 <script>
 export default {
-    
+    props: {
+        card: Array
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-    div {
-        width: auto;
+@import './style/variables.scss';
+
+    .container {
+        width: 1200px;
+        margin: 0 auto;
+        display: flex;
+        flex-wrap: wrap;
+        background-color: $background-darkgrey;
         height: 500px;
-        background-color: #999;
-        text-align: center;
-        line-height: 500px;
+
+        .cards {
+            width: 200px;
+            height: 150px;
+            background-color: red;
+        }
     }
 </style>
