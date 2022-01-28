b99b702d6b578e31138dd5b481a255b4055ad377
Cezar <cezarlantosgmail.com>
2022-01-28 13:51:50 +0100

    Esercizio terminato

--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -1,12 +1,19 @@
 <template>
-    <div class="container">
+    <main>
+        <button class="button-series">CURRENT SERIES</button>
 
-        <div v-for="(card, index) in cards" :key="index" class="cards">
+        <div class="container">
+            
+
+            <div v-for="(card, index) in card" :key="index" class="card">
                 <img :src="card.thumb"/>
-                <h3>{{card.series}}</h3>
+                <span>{{card.series}}</span>
+            </div>
+
         </div>
 
-    </div>
+        <button>LOAD MORE</button>
+    </main>
 </template>
 
 <script>
@@ -20,18 +27,51 @@ export default {
 <style lang="scss" scoped>
 @import './style/variables.scss';
 
-    .container {
-        width: 1200px;
-        margin: 0 auto;
-        display: flex;
-        flex-wrap: wrap;
+    main {
         background-color: $background-darkgrey;
-        height: 500px;
+        color: $text-white-color;
+        padding: 70px 0;
+        text-align: center;
 
-        .cards {
-            width: 200px;
-            height: 150px;
-            background-color: red;
+        .container {
+            width: 1200px;
+            height: 700px;
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+
+
+            .card {
+                width: 15%;
+                padding: 15px;
+                text-align: center;
+
+                img {
+                    width: 100%;
+                }
+            }
+        }
+
+        button {
+        cursor: pointer;
+        text-align: center;
+        outline: none;
+        text-decoration: none;
+        border-style: hidden;
+        padding: 10px 45px;
+        color: $text-white-color;
+        background-color: $background-blu;
+        font-weight: bold;
+        }
+
+        .button-series{
+            padding: 15px;
+            font-size: 25px;
+            position: absolute;
+            top: 320px;
+            left: 50px;
         }
     }
+
+
 </style>
